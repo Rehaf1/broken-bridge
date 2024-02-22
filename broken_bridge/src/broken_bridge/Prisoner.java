4f991bf5808c103d4253568b4d91506dd1b7c74f
rehaf <rehaf@Rehafs-Tablet>
2024-02-22 20:22:09 +0300

    class prisoner

---
+++ broken_bridge/src/broken_bridge/Prisoner.java
@@ -0,0 +1,16 @@
+package broken_bridge;
+
+public class Prisoner {
+	private String serialNumber ;
+	private int timeDue;
+	private String crimeType ;
+	private String cellNumber ; 
+	private int strikes ; 
+	
+	public boolean needsMedication () {}
+	public void privlages(){}
+	public int getStrikes() { return strikes ;  }
+	public void setStrikes(int nStrikes) {strikes = nStrikes;}  
+	
+
+}
