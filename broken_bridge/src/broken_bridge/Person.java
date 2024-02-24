c4950a4fe3a819ca3ce3fc1faea9c0ae0e92db91
rehaf <rehaf@Rehafs-Tablet>
2024-02-24 14:05:49 +0300

    added constructors

--- broken_bridge/src/broken_bridge/Person.java
+++ broken_bridge/src/broken_bridge/Person.java
@@ -1,6 +1,12 @@
 package broken_bridge;
 
-public class Person {
+ public class Person {
  protected String name; 
  protected String id ; 
+
+ public Person(String n , String iD) {
+	name = n ; 
+	id = iD ; 
 }
+ public Person(){ this("unnamed","without Id");
+}}
